619ed8705d538550799e87be7ccbb9d02735ea92
sameer126 <sameer252664@gmail.com>
2023-12-22 23:33:44 +0530

    day - 18

---
+++ Day - 15/detection/app.py
@@ -0,0 +1,45 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+sc_model = pickle.load(open('models/scaler.pkl', 'rb'))
+random_forest_model = pickle.load(open('models/random_forest.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    # return request.form.values()
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    final_scaled = sc_model.transform(final_features)
+    prediction = random_forest_model.predict(final_features)
+
+    # output/= round(prediction[0], 2)
+    if prediction == 1:
+        answer = 'Disease'
+    else :
+        answer = 'NO Disease'
+    return render_template('index.html', prediction_text = answer  )
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
